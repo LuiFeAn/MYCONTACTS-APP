7640bca8691c699724139c7765068a529b877f20
Luis Fernando <luisfernandogvv@gmail.com>
2022-10-25 18:16:10 -0300

    Adicionando States

--- src/components/ContactsForm/index.js
+++ src/components/ContactsForm/index.js
@@ -1,4 +1,5 @@
 import { Form, ButtonContainer } from "./style";
+import { useState } from "react";
 import PropTypes from 'prop-types';
 import FormGroup from "../FormGroup";
 import Input from '../Input';
@@ -6,17 +7,26 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonLabel }){
+
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [phone,setPhone] = useState('');
+
+    const handleName = event => setName(event.target.value);
+    const handleEmail = event => setEmail(event.target.value);
+    const handlePhone = event => setPhone(event.target.value);
+
+
     return(
        <Form>
-
          <FormGroup>
-            <Input placeholder="Nome"/>
+            <Input value={name} onChange={handleName} placeholder="Nome"/>
          </FormGroup>
          <FormGroup error="O formato do e-mail é inválido">
-            <Input error placeholder="E-mail"/>
+            <Input value={email} onChange={handleEmail} error placeholder="E-mail"/>
          </FormGroup>
          <FormGroup>
-            <Input placeholder="Telefone"/>
+            <Input value={phone} onChange={handlePhone} placeholder="Telefone"/>
          </FormGroup>
          <FormGroup>
             <Select>
