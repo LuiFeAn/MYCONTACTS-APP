ec5db4eb543b85b8286e8b5a040649a3edbc0eae
Luis Fernando <luisfernandogvv@gmail.com>
2022-10-26 18:38:26 -0300

    Refatorando estilização do Componente de input

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,26 +3,23 @@ import styled, { css } from "styled-components";
 export default styled.input`
 
     width: 100%;
-    background: #fff;
-    border: 2px solid #fff;
-    box-shadow: 0px 4px 10px rgba(0,0,0,0.04);
     height: 52px;
-    border-radius: 4px;
     padding: 0 16px;
+    background: #fff;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    border-radius: 4px;
     border: 2px solid #fff;
-    font-size:16px;
+    outline: 0;
+    font-size: 16px;
     transition: border-color 0.2s ease-in;
 
     &:focus{
-        outline: 2px solid ${({theme}) => theme.colors.primary.main};
-
+        border-color: ${({ theme }) => theme.colors.primary.main};
     }
 
-    ${({theme,error}) => error && css`
-
-        color:${theme.colors.danger.main};
+    ${({ theme, error }) => error && css`
+        color: ${theme.colors.danger.main};
         border-color: ${theme.colors.danger.main} !important;
-
     `}
 
 `;
