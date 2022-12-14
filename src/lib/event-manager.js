918fd978cc84f5cdfd3b33618d5f10959f017ace
luis <luisfernandogvv@gmail.com>
2022-12-14 18:26:53 -0300

    refactor: :fire: Substituindo a sitaxe de objetos pela classe Map
    Utilizando a classe Map para manipular informações do EventManager

--- src/lib/event-manager.js
+++ src/lib/event-manager.js
@@ -3,26 +3,26 @@
 export default class EventManager {
 
     constructor() {
-        this.listeners = [];
+        this.listeners = new Map();
     }
 
     on(event,listener) {
 
-       if( !this.listeners[event] ) {
-        this.listeners[event] = [];
+       if( !this.listeners.has(event)) {
+            this.listeners.set(event,[]);
        }
 
-       this.listeners[event].push(listener);
+       this.listeners.get(event).push(listener);
 
     }
 
     emit(event,payload) {
 
-        if( !this.listeners[event] ){
+        if( !this.listeners.has(event) ){
             return;
         }
 
-        this.listeners[event].forEach( listener => {
+        this.listeners.get(event).forEach( listener => {
             listener(payload);
         });
 
@@ -32,7 +32,7 @@ export default class EventManager {
 
         const listeners = this.listeners[event];
 
-        if( !this.listeners[event] ){
+        if( !this.listeners.get(event) ){
             return;
         }
 
@@ -40,7 +40,7 @@ export default class EventManager {
             listener != offListener
         ));
 
-        this.listeners[event] = filtredListeners;
+        this.listeners.set(event,filtredListeners);
 
     }
 
