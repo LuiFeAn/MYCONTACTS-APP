b7859b302bc2bbf87e9314ce13a42f15b32f68d1
luis <luisfernandogvv@gmail.com>
2022-12-14 17:58:21 -0300

    feat: :fire: Criando um EventManager para os toasts
    Implementando a classe EventManager, que tem por fim ser um gerenciador de eventos semelhante ao da addEventListener

---
+++ src/lib/event-manager.js
@@ -0,0 +1,43 @@
+
+
+export default class EventManager {
+
+    constructor() {
+        this.listeners = [];
+    }
+
+    on(event,listener) {
+
+       if( !this.listeners[event] ) {
+        this.listeners[event] = [];
+       }
+
+       this.listeners[event].push(listener);
+
+    }
+
+    emit(event,payload) {
+
+        if( !this.listeners[event] ){
+            return;
+        }
+
+        this.listeners[event].forEach( listener => {
+            listener(payload);
+        });
+
+    }
+
+}
+
+const toastEventManager = new EventManager();
+
+toastEventManager.on('addtoast', (payload) => {
+    console.log(payload);
+});
+
+toastEventManager.emit('addtoast', {
+    type:'danger',
+    text:'texto'
+});
+
